1b725fcedcdb5d352e5233c02e16532ffdf0949c
farisafathima <148028476+farisafathima@users.noreply.github.com>
2024-02-09 19:56:48 +0530

    KMP_PREFIX_FUNCTION.py

--- KMP_PREFIX_FUNCTION.py
+++ KMP_PREFIX_FUNCTION.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def prefix_function(P):
     m = len(P)
-    pi = [0] * n
+    pi = [0] * m
     for q in range(1, m):
         k = pi[q-1]
         while k > 0 and P[q] != P[k]:
